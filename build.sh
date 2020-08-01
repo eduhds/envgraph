2a8fdc717f5179f5669ee65e858f6c05cc42814e
Eduardo <eduardohds.dev@outlook.com>
2020-08-01 16:04:45 -0300

    Install test

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 rm -rf dist
 mkdir dist
 
+cp *.js ./dist
+cp -r node_modules ./dist/node_modules
+
 echo "[{\"name\":\"LEVE_HOME\",\"value\":\"\$HOME/.leve\"}]" >> ./dist/vars.json
 echo "export PATH=\"\$PATH:\$LEVE_HOME\"" >> ./dist/vars.leve
--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 
 SHELL_PROFILE=$HOME/.bash_profile
 LEVE_HOME=$HOME/.leve
+NODE=$(which node)
 
 rm -rf $LEVE_HOME
 
 mkdir $LEVE_HOME
 
-cp ./dist/leve $LEVE_HOME/leve
-cp ./dist/vars.json $LEVE_HOME/vars.json
-cp ./dist/vars.leve $LEVE_HOME/vars.leve
+cp -r ./dist/* $LEVE_HOME
 
 echo "source $HOME/.leve/vars.leve" >> $SHELL_PROFILE
+echo "alias leve='$NODE $LEVE_HOME'" >> $SHELL_PROFILE
 
 source $SHELL_PROFILE
