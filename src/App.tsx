8318d8d3e92eef863d7502daf04d258bdbea73ac
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-01-22 19:26:38 -0300

    feat: reaflow added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {} from 'react';
+import { Canvas } from 'reaflow';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Canvas
+        maxWidth={800}
+        maxHeight={600}
+        direction='RIGHT'
+        disabled
+        nodes={[
+          {
+            id: '1',
+            text: '1'
+          },
+          {
+            id: '2',
+            text: '2'
+          }
+        ]}
+        edges={[
+          {
+            id: '1-2',
+            from: '1',
+            to: '2'
+          }
+        ]}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
