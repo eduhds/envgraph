c15b945379ff0946d68a2c38854e05f10f1420fc
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-01-24 11:26:18 -0300

    fix: app updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,67 @@
-import {} from 'react';
+import { useEffect, useState } from 'react';
 import { Canvas } from 'reaflow';
 
-import './App.css';
+type Nodes = { id: string; text: string }[];
+type Edges = { id: string; from: string; to: string }[];
 
 function App() {
+  const [nodes, setNodes] = useState<Nodes>([]);
+  const [edges, setEdges] = useState<Edges>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8090/api/env')
+      .then(res => res.json())
+      .then(res => {
+        const envs: Array<string> = res.envs;
+
+        let _nodes: Nodes = [];
+        let _edges: Edges = [];
+
+        for (const env of envs) {
+          const segments = env
+            .trim()
+            .split('/')
+            .filter(s => !!s);
+
+          let key = segments.length;
+
+          for (const seg of [...segments].reverse()) {
+            const id = segments.slice(0, key).join('/');
+
+            if (_nodes.find(n => n.id === id)) {
+              break;
+            }
+
+            _nodes.push({ id, text: seg });
+
+            if (key > 1) {
+              const prev = segments.slice(0, key - 1).join('/');
+
+              _edges.push({
+                id: prev + '-' + id,
+                from: prev,
+                to: id
+              });
+            }
+
+            key--;
+          }
+        }
+
+        setNodes(_nodes);
+        setEdges(_edges);
+      });
+  }, []);
+
   return (
     <div>
       <Canvas
-        maxWidth={800}
-        maxHeight={600}
+        //maxWidth={800}
+        //maxHeight={600}
         direction='RIGHT'
         disabled
-        nodes={[
-          {
-            id: '1',
-            text: '1'
-          },
-          {
-            id: '2',
-            text: '2'
-          }
-        ]}
-        edges={[
-          {
-            id: '1-2',
-            from: '1',
-            to: '2'
-          }
-        ]}
+        nodes={nodes}
+        edges={edges}
       />
     </div>
   );
